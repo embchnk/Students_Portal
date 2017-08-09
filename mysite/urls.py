f19d77b95a0059164f0d387e5aee07c9b80a4e87
mgacek8 <mgacek8@wp.pl>
2017-08-09 20:30:16 +0200

    preparing users registration

--- mysite/urls.py
+++ mysite/urls.py
@@ -13,9 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    # when user request anything that starts with users - go to users.urls to find out what to do
+    url(r'^users/', include('users.urls')),
 ]
---
+++ users/views.py
@@ -0,0 +1,46 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.views.generic import View
+from .forms import UserForm
+
+def index(request):
+    return HttpResponse("<h1>This is the User app homepage...</h1>")
+
+class UserFormView(View):
+    #blueprint
+    form_class = UserForm
+    # html file
+    template_name = 'users/registration_form.html'
+
+    # display blank form to the user
+    def get(self, request):
+        form = self.form_class(None)
+        return render(request, self.template_name, {'form':form})
+
+    # process form data
+    def post(self, request):
+        form = self.form_class(request.POST) # whenever user hits submit, all of that gets stored in request.POST
+
+        if form.is_valid():
+            # creates an object from form but doesnt save it to database yet
+            user = form.save(commit=False)
+            # now clean/normalized data
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user.set_password(password)
+            user.save()
+
+            # returns User object if credentials are correct
+            user = authenticate(username=username, password=password)
+
+            if user is not None:
+                # not banned, disabled, etc.
+                if user.is_active:
+                    login(request, user)
+                    # request.user.(username,profilephoto, etc) - now we can reffer to user whenever we want
+                    redirect('users:index')
+
+            return render(request, self.template_name, {'form':form})
