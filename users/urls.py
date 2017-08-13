85c2d7e590d0a22688e1f633272caf272d73d42c
radoslaw <radoslaw.kumorekit@gmail.com>
2017-08-13 18:22:45 +0200

    My first commit, it is not the end

--- users/urls.py
+++ users/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^register/$', views.UserFormView.as_view(), name='register'),
+    url(r'^new_recipe/$', views.new_recipe, name='new_recipe'),
+    url(r'^result/$', views.result_of_addition_recipe, name='result_of_addition_recipe'),
+    url(r'^bookRecipes/$', views.bookRecipes, name='bookRecipes'),
+    url(r'^bookRecipes/(?P<recipe_id>[0-9]+)$', views.single_recipe, name='single_recipe')
 ]
