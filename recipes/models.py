84125f03531d60089a0646f98dee45389fee44a1
Michał Bochnak <baniak996@gmail.com>
2017-08-20 14:01:02 +0200

    Change adding and displaying recipes method
    Add new class Quantity contatinig Value and connected to recipe and ingredient

--- recipes/models.py
+++ recipes/models.py
@@ -17,15 +17,26 @@ class Recipe(models.Model):
 class Ingredient(models.Model):
     name = models.CharField(max_length=100)
     recipe = models.ManyToManyField(Recipe)
-    how_many = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.name+' - '+self.how_many
-
-
-class Quantity(models.Model):
-    unit = models.CharField(max_length=100)
-    ingredient = models.ManyToManyField(Ingredient)
 
     def __str__(self):
         return self.name
+
+
+class Unit(models.Model):
+    unit = models.CharField(max_length=100)
+    recipe = models.ManyToManyField(Recipe)
+    ingredient = models.ManyToManyField(Ingredient)
+
+    def __str__(self):
+        return self.unit
+
+
+class Quantity(models.Model):
+    value = models.IntegerField()
+    recipe = models.ManyToManyField(Recipe)
+    ingredient = models.ManyToManyField(Ingredient)
+
+    def __str__(self):
+        return str(self.value)
+
+
--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,4 @@
-from .models import Recipe, Ingredient
+from .models import Recipe, Ingredient, Unit, Quantity
 from users.models import Profile, User
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponseNotFound
@@ -22,7 +22,10 @@ def single_recipe(request, recipe_id):
     # except Recipe.DoesNotExist:
     #    raise Http404
     single_recipe = get_object_or_404(Recipe, pk=recipe_id)
-    return render(request, 'recipes/single_recipe.html', {'single_recipe': single_recipe})
+    dict = {}
+    for i in range(single_recipe.ingredient_set.count()):
+        dict[single_recipe.ingredient_set.all()[i]] = str(single_recipe.unit_set.all()[i]) + " " + str(single_recipe.quantity_set.all()[i])
+    return render(request, 'recipes/single_recipe.html', {'single_recipe': single_recipe, 'dict': dict})
 
 
 def new_recipe(request):
@@ -40,18 +43,28 @@ def result_of_addition_recipe(request):
             new_recipe = Recipe(author=profile, title=request.POST['name'], likes=0, instruction=request.POST['instruction'], level=request.POST['level'])
             new_recipe.save()
             for i in range(int(request.POST['number']) + 1):
-                temp_i = 'i'+str(i)
-                temp_how_many = 'how_many'+str(i)
-                # now i check if ingredient is located in our database
-                how_many = "none"
-                if request.POST[temp_how_many] is not None:
-                    how_many = request.POST[temp_how_many]
-                if check_database(request.POST[temp_i], how_many) is None:
-                    ingredient = Ingredient(name=request.POST[temp_i], how_many=how_many)
+                ingredient_index = "i" + str(i)
+                unit_index = "how_many" + str(i)
+                try:
+                    ingredient = Ingredient.objects.get(Q(name=ingredient_index))
+                except Ingredient.DoesNotExist:
+                    ingredient = Ingredient(name=request.POST[ingredient_index])
                     ingredient.save()
-                else:
-                    ingredient = check_database(request.POST[temp_how_many], how_many)
+                try:
+                    unit = Unit.objects.get(Q(unit="grams"))
+                except Unit.DoesNotExist:
+                    unit = Unit(unit="grams")
+                    unit.save()
+                try:
+                    quantity = Quantity.objects.get(Q(value=request.POST[unit_index]))
+                except Quantity.DoesNotExist:
+                    quantity = Quantity(value=request.POST[unit_index])
+                    quantity.save()
                 ingredient.recipe.add(new_recipe)
+                unit.recipe.add(new_recipe)
+                unit.ingredient.add(ingredient)
+                quantity.recipe.add(new_recipe)
+                quantity.ingredient.add(ingredient)
             return render(request, 'recipes/recipe_form.html', {'result': 'True'})
         else:
             return render(request, 'recipes/recipe_form.html', {'result': 'False'})
@@ -59,9 +72,9 @@ def result_of_addition_recipe(request):
         return redirect('users:index')
 
 
-def check_database(name_of, how_many):
+def check_database(name_of):
     try:
-        if Ingredient.objects.get(Q(name=name_of) & Q(how_many=how_many)) is not None:
-            return Ingredient.objects.get(Q(name=name_of) & Q(how_many=how_many))
+        if Ingredient.objects.get(Q(name=name_of)) is not None:
+            return Ingredient.objects.get(Q(name=name_of))
     except Ingredient.DoesNotExist:
         return None
