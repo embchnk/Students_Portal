2ab37ee16582ce80de3f45d7079e9331515641c9
Michał Bochnak <baniak996@gmail.com>
2017-09-02 17:47:29 +0200

    Add simple tests in recipes/tests
    To test write: python manage.py test recipes.tests

--- users/forms.py
+++ users/forms.py
@@ -1,15 +1,28 @@
 from django.contrib.auth.models import User
 from django import forms
 from django.contrib.auth.forms import PasswordChangeForm
+from django.utils.translation import ugettext_lazy as _
 
 
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput)
     password_confirm = forms.CharField(widget=forms.PasswordInput)
 
+    # example of how to translate both password fields to Polish language
+    # password = forms.CharField(widget=forms.PasswordInput, label='Hasło')
+    # password_confirm = forms.CharField(widget=forms.PasswordInput, label='Potwierdź hasło')
+
     class Meta:
         model = User
-        fields = ['username', 'email', 'first_name', 'last_name', 'password',]
+        fields = ['username', 'email', 'first_name', 'last_name', 'password']
+
+        # example of how to translate form_fields to Polish language
+        # labels = {
+        #     'username': _('Nazwa użytkownika'),
+        #     'email': _('Adres e-mail'),
+        #     'first_name': _('Imię'),
+        #     'last_name': _('Nazwisko'),
+        # }
 
 
 class LoginForm(forms.ModelForm):
