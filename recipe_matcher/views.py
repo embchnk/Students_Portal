6f9e6c0dbb3bb3ceec8bceb081e96327a667bce1
Michał Bochnak <baniak996@gmail.com>
2017-09-04 00:00:28 +0200

    Add view to future template showing missing ingredients
    recipe_matcher/views

--- recipe_matcher/views.py
+++ recipe_matcher/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from recipes.models import *
 
 
@@ -11,4 +11,18 @@ def match_recipes(request):
                 matched_recipes[recipe] += 1
             except KeyError:
                 matched_recipes[recipe] = 1
+    request.session['ingredients'] = result
     return render(request, 'recipes/bookRecipes.html', {'all_recipes': reversed(sorted(matched_recipes, key=matched_recipes.__getitem__))})
+
+
+# view similar to single_recipe but also storing information about missing_ingredients (hope it works)
+# need to change template to one showing missing_ingredients list
+def matched_recipe(request, recipe_id):
+    single_recipe = get_object_or_404(Recipe, pk=recipe_id)
+    ingredients = request.session.pop('ingredient', None)
+    request.session.modified = True
+    missing_ingredients = []
+    for ingredient in Ingredient.objects.get(recipe=single_recipe):
+        if ingredient not in ingredients:
+            missing_ingredients.append(ingredient)
+    return render(request, 'recipes/single_recipe.html', {'missing_ingredients': missing_ingredients})
