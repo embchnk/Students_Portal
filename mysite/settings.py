69d9844b31a513567629bd526099826cf5a2bffd
Michał Bochnak <baniak996@gmail.com>
2017-09-21 15:27:11 +0200

    Add Allowed-hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'x^@hc+bgc*x$vx99&-z+8m!)4-lgdtcq=38w#$57ev0l%&-=&8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
